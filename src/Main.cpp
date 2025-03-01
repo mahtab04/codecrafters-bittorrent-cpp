a6f3027558a961a5221b5d4c306093ffd851a878
Mahtab Alam <smmalam9@gmail.com>
2025-03-01 14:17:14 +0000

    integer parsing

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,21 +8,49 @@
 
 using json = nlohmann::json;
 
-json decode_bencoded_value(const std::string& encoded_value) {
-    if (std::isdigit(encoded_value[0])) {
+json decode_bencoded_value(const std::string& encoded_value) 
+{
+
+    if (std::isdigit(encoded_value[0]))
+    {
+
         // Example: "5:hello" -> "hello"
+
         size_t colon_index = encoded_value.find(':');
-        if (colon_index != std::string::npos) {
+
+        if (colon_index != std::string::npos) 
+        {
             std::string number_string = encoded_value.substr(0, colon_index);
+
             int64_t number = std::atoll(number_string.c_str());
+
             std::string str = encoded_value.substr(colon_index + 1, number);
+
             return json(str);
-        } else {
-            throw std::runtime_error("Invalid encoded value: " + encoded_value);
+
         }
-    } else {
-        throw std::runtime_error("Unhandled encoded value: " + encoded_value);
+        else
+        {
+
+            throw std::runtime_error("Invalid encoded value: " + encoded_value);
+
+        }
+
     }
+    else if (encoded_value[0] == 'i' && encoded_value[encoded_value.size()-1] == 'e')
+    {
+
+        std::string x = encoded_value.substr(1, encoded_value.size()-2);
+
+        return json(std::atoll(x.c_str()));
+    } 
+    else
+    {
+
+        throw std::runtime_error("Unhandled encoded value: " + encoded_value);
+
+    }
+
 }
 
 int main(int argc, char* argv[]) {
